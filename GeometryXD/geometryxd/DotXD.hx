7bf51d3f2e14fc01469c60c29dac66a9eb8b8cb4
healingdrawing <healingdrawing@gmail.com>
2020-04-23 03:08:38 +0300

    DotXD in process

--- GeometryXD/geometryxd/DotXD.hx
+++ GeometryXD/geometryxd/DotXD.hx
@@ -1,35 +1,88 @@
 package geometryxd;
 
+/**
+ * multidimensional dot object
+ */
 class DotXD{
+    /**
+     * array of dot coordinates
+     */
     var value:Array<Float>;
     
     /**
      * new multidimentional dot object.
      * @param dotXD - array of dot coordinates
      */
-    public function new(dotXD:Array<Float>) {
+     public function new(dotXD:Array<Float>) {
         value = dotXD;
     }
     
-    //public static function maxabs(a:Array<Float>):Float last before geometry functions section
+    /**
+     * number or dot dimensions
+     */
+    public var dn(get,never):Int;
+    function get_dn() {
+        return this.value.length;
+    }
     
-    function oposite():Array<Float>{return GeometryXD.minus_F(value);}
+    /**
+     * array of mirrored values of dot coordinates. If `value` = `[1,2,3,-4]` then `valueM` = `[-1,-2,-3,4]`
+     */
+    public var valueM(get,never):Array<Float>;
+    function get_valueM(){
+        return GeometryXD.minus_F(value);
+    }
 }
 
 class Dot3D{
+    /**
+     * x coordinate of dot
+     */
     var x:Float;
+    /**
+     * y coordinate of dot
+     */
     var y:Float;
+    /**
+     * z coordinate of dot
+     */
     var z:Float;
-    var value:Array<Float>;
     
     /**
      * new 3D dot object.
-     * @param dot3D - array of dot coordinates `[x,y,z]`
+     * @param x coordinate of dot
+     * @param y coordinate of dot
+     * @param z coordinate of dot
      */
-     public function new(dot3D:Array<Float>) {
-        x = dot3D[0]; y=dot3D[1]; z=dot3D[2];
-        value = [x,y,z];
+    public function new(x:Float, y:Float, z:Float) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
     
-    function oposite():Array<Float>{return GeometryXD.minus_F(value);}
+    /**
+     * array of dot coordinates `[x,y,z]`
+     */
+    var value(get,never):Array<Float>;
+    function get_value() {
+        return [this.x,this.y,this.z];
+    }
+    /**
+     * number or dot dimensions
+     */
+    public var dn(get,never):Int;
+    function get_dn() {
+        return this.value.length;
+    }
+    
+    /**
+     * array of mirrored values of dot coordinates. If `value` = `[1,2,3,-4]` then `valueM` = `[-1,-2,-3,4]`
+     */
+    public var valueM(get,never):Array<Float>;
+    function get_valueM(){
+        return GeometryXD.minus_F(value);
+    }
+    
 }
+
+//public static function maxabs(a:Array<Float>):Float last before geometry functions section
