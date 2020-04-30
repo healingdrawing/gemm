457a15c49eba6e3c1b669cbd4d52fa0f6b9d74df
healingdrawing <healingdrawing@gmail.com>
2020-05-01 00:22:12 +0300

    Angle.hx in process

--- GeometryXD/geometryxd/Angle.hx
+++ GeometryXD/geometryxd/Angle.hx
@@ -146,78 +146,160 @@ class Angle {
      * get angle value as `turn`
      */
      public function turn() {
-        return this.value;
+        return value;
         }
     
     /**
      * get angle value as `multiples of Math.PI`
      */
     public function mulp() {
-        return this.value * 2;
+        return value * 2;
     }
     
     /**
      * get angle value as `quadrant`
      */
     public function quad() {
-        return this.value * 4;
+        return value * 4;
     }
     
     /**
      * get angle value as `sextant`
      */
     public function sext() {
-        return this.value * 6;
+        return value * 6;
     }
     
     /**
      * get angle value as `radian`
      */
     public function rad() {
-        return this.value * 2 * Math.PI;
+        return value * 2 * Math.PI;
     }
     
     /**
      * get angle value as `hexacontade`
      */
     public function hexa() {
-        return this.value * 60;
+        return value * 60;
     }
     
     /**
      * get angle value as `binary degree`
      */
     public function bdeg() {
-        return this.value * 256;
+        return value * 256;
     }
     
     /**
      * get angle value as `degree`
      */
     public function deg() {
-        return this.value * 360;
+        return value * 360;
     }
     
     /**
      * get angle value as `grad`
      */
     public function grad() {
-        return this.value * 400;
+        return value * 400;
     }
     
     /**
      * get angle value as `minute of arc`
      */
     public function marc() {
-        return this.value * 21600;
+        return value * 21600;
     }
     
     /**
      * get angle value as `second of arc`
      */
     public function sarc() {
-        return this.value * 1296000;
+        return value * 1296000;
     }
+    
+    //sin cos section
+    /** trigonometric sine of angle */
+    public function sin() { return Math.sin(rad()); }
+    /** trigonometric cosine of angle */
+    public function cos() { return Math.cos(rad()); }
+    /** trigonometric tangent of angle */
+    public function tan() { return Math.tan(rad()); }
+    /** trigonometric cotangent of angle */
+    public function cot() { return 1/tan(); }
+    /** trigonometric secant of angle */
+    public function sec() { return 1/cos(); }
+    /** trigonometric cosecant of angle */
+    public function csc() { return 1/sin(); }
+    
+    /** hyperbolic sine of angle */
+    public function sinh() { var a = rad(); return (Math.exp(a) - Math.exp(-a))/2; }
+    /** hyperbolic cosine of angle */
+    public function cosh() { var a = rad(); return (Math.exp(a) + Math.exp(-a))/2; }
+    /** hyperbolic tangent of angle */
+    public function tanh() { return sinh()/cosh(); }
+    /** hyperbolic cotangent of angle */
+    public function coth() { return cosh()/sinh(); }
+    /** hyperbolic secant of angle */
+    public function sech() { return 1/cosh(); }
+    /** hyperbolic cosecant of angle */
+    public function csch() { return 1/sinh(); }
+    
+    //use sin cos section
+    
+    /** set angle value using trigonometric sine of angle 
+     * @param v sine of angle
+    */
+    public function useSin(v:Float) { useRad( Math.asin(v) ); }
+    /** set angle value using trigonometric cosine of angle 
+     * @param v cosine of angle
+    */
+    public function useCos(v:Float) { useRad(Math.acos(v)); }
+    /** set angle value using trigonometric tangent of angle 
+     * @param v tangent of angle
+    */
+    public function useTan(v:Float) { useRad(Math.atan(v)); }
+    /** set angle value using trigonometric cotangent of angle 
+     * @param v cotangent of angle
+    */
+    public function useCot(v:Float) { useRad( Math.atan(v) + ((v<0)?Math.PI:0) ); }
+    /** set angle value using trigonometric secant of angle 
+     * @param v secant of angle
+    */
+    public function useSec(v:Float) { useRad(Math.acos(1/v)); }
+    /** set angle value using trigonometric cosecant of angle 
+     * @param v cosecant of angle
+    */
+    public function useCsc(v:Float) { useRad(Math.asin(1/v)); }
+    
+    /** set angle value using hyperbolic sine of angle 
+     * @param v sine of angle
+    */
+    public function useSinh(v:Float) { useRad( Math.log( v + Math.sqrt(v*v + 1) ) ); }
+    /** set angle value using hyperbolic cosine of angle 
+     * @param v cosine of angle
+    */
+    public function useCosh(v:Float) { useRad( Math.log( v + Math.sqrt(v*v - 1) ) ); }
+    /** set angle value using hyperbolic tangent of angle 
+     * @param v tangent of angle
+    */
+    public function useTanh(v:Float) { useRad( 0.5 * Math.log( (1+v)/(1-v) ) ); }
+    /** set angle value using hyperbolic cotangent of angle 
+     * @param v cotangent of angle
+    */
+    public function useCoth(v:Float) { useRad( 0.5 * Math.log( (v+1)/(v-1) ) ); }
+    /** set angle value using hyperbolic secant of angle 
+     * @param v secant of angle
+    */
+    public function useSech(v:Float) { useRad( Math.log( 1/v + Math.sqrt(1/(v*v) - 1) ) ); }
+    /** set angle value using hyperbolic cosecant of angle 
+     * @param v cosecant of angle
+    */
+    public function useCsch(v:Float) { useRad( Math.log( 1/v + Math.sqrt(1/(v*v) + 1) ) ); }
+    
+    
+    //from sin cos section
     
     //plus minus value section for each unit variant
     
