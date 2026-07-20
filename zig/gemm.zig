65202cd27bbca0f57d04ed94668179a84a9e262b
healingdrawing <healingdrawing@gmail.com>
2026-07-21 01:41:32 +0300

    add raw testing skeleton of default compiled libs in zig-out/lib

--- zig/gemm.zig
+++ zig/gemm.zig
@@ -17,12 +17,13 @@ pub fn v3v3scalar(v3a: []const f32, v3b: []const f32) f32 {
 
 // --- FROM varzig/varzig.zig ---
 pub fn varzig() void {
-    const v3: [3]u8 = .{ 1, 3, 4 };
-    callme(v3);
+    //todo it should fail some way when lib tested since const used, keep it for now
+    var v3: [3]u8 = .{ 1, 3, 4 };
+    callme(&v3);
     return v3;
 }
 
-pub fn callme(v3: [3]u8) void {
+pub fn callme(v3: *[3]u8) void {
     v3[0] = 2;
 }
 
