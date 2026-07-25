faf9baa5f07fc3761f72980dc142092e9c17b45d
healingdrawing <healingdrawing@gmail.com>
2026-07-25 23:09:53 +0300

    reminder . plane abcd d requires to be scaled properly

---
+++ ts/plane.ts
@@ -0,0 +1,9 @@
+//todo remove later. raw test/reminder
+import { GEMM } from "./gemm";
+const gemm= new GEMM()
+const d3 = [6, 6, 6] as unknown as Float32Array;
+const p31 = [1, 1, 1, 9] as unknown as Float32Array;
+const p32 = [10, 10, 10, 9] as unknown as Float32Array;
+console.log(gemm.distance_d3_p3(d3, p31))
+console.log(gemm.distance_d3_p3(d3, p32))
+// in case of scale of the plane (maybe to rebalance number or so) the d must be scaled too
