8c62f08c12d088169158a92d2292d8917a67ad38
healingdrawing <healingdrawing@gmail.com>
2026-07-30 15:35:58 +0300

    remove artefact

--- zig/gemm.zig
+++ zig/gemm.zig
@@ -67,18 +67,4 @@ pub inline fn v3v3scalar(a: @Vector(3, f32), b: @Vector(3, f32)) f32 {
 }
 
 
-
-// --- FROM varzig/varzig.zig ---
-pub fn varzig() void {
-    //todo it should fail some way when lib tested since const used, keep it for now
-    var v3: [3]u8 = .{ 1, 3, 4 };
-    callme(&v3);
-    return v3;
-}
-
-pub fn callme(v3: *[3]u8) void {
-    v3[0] = 2;
-}
-
-
 };
